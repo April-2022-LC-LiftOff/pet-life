8868c3ca683cb8035bc69e38b49221a5720de6c0
evan812 <61596243+evan812@users.noreply.github.com>
2022-04-18 20:20:44 -0500

    Spring int

---
+++ src/main/java/org/launchcode/PetLife/PetLifeApplication.java
@@ -0,0 +1,13 @@
+package org.launchcode.PetLife;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetLifeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetLifeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/launchcode/PetLife/PetLifeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.launchcode.PetLife;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetLifeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
