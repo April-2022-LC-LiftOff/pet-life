53b82d3e8d1ee96ef4f7c96e21efa80be4faeb98
Ridgeway22 <boo22mickey@yahoo.com>
2022-05-02 12:58:01 -0500

    git commit -m "login/registration"

---
+++ src/main/java/org/launchcode/PetLife/controllers/AppController.java
@@ -0,0 +1,51 @@
+package org.launchcode.PetLife.controllers;
+
+import org.launchcode.PetLife.models.User;
+import org.launchcode.PetLife.models.data.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+//@RequestMapping("login")
+public class AppController {
+    @Autowired
+    private UserRepository userRepo;
+
+    @GetMapping("/login")
+    public String viewHomePage() {
+        return "login";
+    }
+
+    @GetMapping("/register")
+    public String showRegistrationForm(Model model) {
+        model.addAttribute("user", new User());
+
+        return "register";
+
+    }
+
+    @PostMapping("/register")
+    public String processRegister(User user) {
+        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        String encodedPassword = passwordEncoder.encode(user.getPassword());
+        user.setPassword(encodedPassword);
+
+        userRepo.save(user);
+
+        return "login";
+    }
+//    @GetMapping("/users")
+//    public String listUsers(Model model) {
+//        List<User> listUsers = userRepo.findAll();
+//        model.addAttribute("listUsers", listUsers);
+//
+//        return "users";
+//    }
+}
