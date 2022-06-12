8b58844700b45ea906eb9483d352ff7496cfe7c2
Jenhsiang Hsieh (Wren) <wrenhsieh18@gmail.com>
2022-06-12 11:26:38 -0500

    add google map autocomplete search API

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -93,24 +93,18 @@ init = () => {
         )
     }
 
-
     const showSlides = () => {
         let i;
         const slides = document.getElementsByClassName("mySlides");
-//        let dots = document.getElementsByClassName("dot");
+
         if (slides !== null) {
             for (i = 0; i < slides.length; i++) {
                 slides[i].style.display = "none";
             }
             slideIndex++;
             if (slideIndex > slides.length) {slideIndex = 1}
-//        for (i = 0; i < dots.length; i++) {
-//            dots[i].className = dots[i].className.replace(" active", "");
-//        }
-
             slides[slideIndex-1].style.display = "block";
-//            dots[slideIndex-1].className += " active";
-            setTimeout(showSlides, 2000); // Change image every 2 seconds
+            setTimeout(showSlides, 2000);
         }
 
 
